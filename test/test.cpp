a012aa2d31d8257ee0a6103002dae4b19c4bbf54
lk <448312134@qq.com>
2021-09-25 22:14:48 +0800

    更新构建配置与测试样例

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,4 @@
-#include "../src/TaskScheduler/TimedTaskScheduler.h"
-#include "../src/ThreadPool/ThreadPool.h"
+#include "../src/TimedTaskExcutor.h"
 
 #include <iostream>
 #include <thread>
@@ -11,24 +10,40 @@ int callBack(int i, int *result) {
 }
 
 int main() {
-  TimedTaskScheduler timer;
+  TimedTaskExcutor excutor;
+
+  // TimedTaskScheduler timer;
   int i = 0;
   int result = 0;
-  auto a = timer.addTaskWithFuture(5000, callBack, i++, &result);
-  auto b = timer.addTaskWithFuture(14000, callBack, i++, &result);
-  auto c = timer.addTaskWithFuture(7000, callBack, i++, &result);
-  auto id = timer.addTaskWithFuture(10000, ::callBack, i++, &result);
-  timer.adjustTask(id.first, -1000);
+  auto a = excutor.addTaskWithFuture(5000, callBack, i++, &result);
+  auto b = excutor.addTaskWithFuture(14000, callBack, i++, &result);
+  auto c = excutor.addTaskWithFuture(7000, callBack, i++, &result);
+  auto id = excutor.addTaskWithFuture(10000, ::callBack, i++, &result);
+  // timer.adjustTask(id.first, -1000);
 
-  auto pool = ThreadPool::getInstance();
-  pool->submit([&timer]() {
-    while (timer.getTaskCount() > 0) {
-      timer.getNextTickInterval();
-      // std::cout << "next task timeout: " << timer.getNextTickInterval() <<
-      // std::endl;
-    }});
+  // auto pool = ThreadPool::getInstance();
+  // pool->submit([&timer]() {
+  //   while (timer.getTaskCount() > 0) {
+  //     timer.getNextTickInterval();
+  //     // std::cout << "next task timeout: " << timer.getNextTickInterval() <<
+  //     // std::endl;
+  //   }});
+  // TaskExcutor::stopExcutor();
+  // while (1) {
+  //   using namespace std::chrono_literals;
+  //   std::this_thread::sleep_for(20000ms);
+  //   break;
+  // }
+  std::cout << id.second.get() << std::endl;
+  std::cout << a.second.get() << std::endl;
 
-  pool->joinAll();
-  //std::cout << id.second.get() << std::endl;
-  //std::cout << a.second.get() << std::endl;
+  auto pp = excutor.addTaskWithFuture(5000, callBack, i++, &result);
+  // while (1) {
+  //   using namespace std::chrono_literals;
+  //   std::this_thread::sleep_for(20000ms);
+  //   break;
+  // }
+
+  TaskExcutor::stopExcutor();
+  ThreadPool::joinAll();
 }
