251cad08ef3b4a7b7eb20c61b021ec7b2be3cdf4
lk <448312134@qq.com>
2021-09-25 22:12:56 +0800

    feature：重构该类。 - 将Scheduler类作为SchedulerImpl类型的上层包装。重新封装相关方法，把void*参数限定为int类型。 - 添加了获取其包装的SchedulerImpl类型指针的方法，供Excutor调用。

--- src/TaskScheduler/TimedTaskScheduler.h
+++ src/TaskScheduler/TimedTaskScheduler.h
@@ -2,48 +2,37 @@
 #define __TIMED_TASK_SCHEDULER__
 
 #include "Impl/TimedScheduler/TimedTaskSchedulerImpl.h"
+#include <memory>
 
 class TimedTaskScheduler {
 private:
-  TimedTaskSchedulerImpl *timer;
+  std::unique_ptr<TimedTaskSchedulerImpl> timedSchedulerBase;
 
 public:
-  TimedTaskScheduler() : timer(new TimedTaskSchedulerImpl()){};
+  TimedTaskScheduler() : timedSchedulerBase(new TimedTaskSchedulerImpl()){};
   ~TimedTaskScheduler() = default;
 
-  // template <typename F, typename... Args>
-  // auto addTaskWithFuture(int timeout, F &&f, Args &&... args)
-  //     -> std::pair<int, std::future<decltype(f(args...))>> {
-  //   auto taskPtr =
-  //   std::make_shared<std::packaged_task<decltype(f(args...))()>>(
-  //       std::bind(std::forward<F>(f), std::forward<Args>(args)...));
-  //   int id = this->timer->addTask([taskPtr]() { (*taskPtr)(); }, &timeout);
-  //   return {id, taskPtr->get_future()};
-  // }
-
-  // template <typename F, typename... Args>
-  // auto addTask(int timeout, F &&f, Args &&... args) -> int {
-  //   auto task = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
-  //   int id = this->timer->addTask(task, &timeout);
-  //   return id;
-  // }
   int addTask(std::function<void()> task, int timeout) {
-    return timer->addTask(task, &timeout);
+    return timedSchedulerBase->addTask(task, &timeout);
   }
 
   int adjustTask(int id, int timeout) {
-    return timer->adjustTask(id, &timeout);
+    return timedSchedulerBase->adjustTask(id, &timeout);
   }
 
-  int removeTask(int id, bool doCall) { return timer->removeTask(id, doCall); }
+  int removeTask(int id, bool doCall) {
+    return timedSchedulerBase->removeTask(id, doCall);
+  }
 
-  void clearTask() { timer->clearTask(); }
+  void clearTask() { timedSchedulerBase->clearTask(); }
 
-  int getTaskCount() { return timer->getTaskCount(); }
+  int getTaskCount() { return timedSchedulerBase->getTaskCount(); }
 
   std::pair<std::vector<std::function<void()>>, int> getReadyTask() {
-    return timer->getReadyTask();
+    return timedSchedulerBase->getReadyTask();
   }
+
+  TaskSchedulerImplBase *getSchedulerBase() { return timedSchedulerBase.get(); }
 };
 
 #endif
