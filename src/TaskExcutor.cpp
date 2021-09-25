630f78549f1bc00f54c00df1e3a06535b6ff0aef
lk <448312134@qq.com>
2021-09-25 22:13:51 +0800

    feature: 添加Task执行器的实现

--- src/TaskExcutor.cpp
+++ src/TaskExcutor.cpp
@@ -1,4 +1,5 @@
 #include "TaskExcutor.h"
+#include "iostream"
 
 std::once_flag TaskExcutor::constructFlag;
 
@@ -7,24 +8,79 @@ void TaskExcutor::polling() {
   while (true) {
     {
       std::unique_lock<std::mutex> ul(this->mtx_);
+      //当等待时间不为0且轮询队列不为空时才跳过等待，或者是停止标志位被设置
       this->cv_.wait_for(ul, minPollingInterval * 1ms, [this]() {
-        return !this->schedulerPolingList.empty();
+        return (minPollingInterval <= 0 &&
+                !this->schedulerPolingList.empty()) ||
+               stop;
       });
-      this->minPollingInterval = INT_MAX;
+      //停止标志位被设置，等待所有任务均调度完成
+      if (stop && this->schedulerPolingList.empty()) {
+        return;
+      }
+
+      this->minPollingInterval = INT32_MAX;
+
       for (auto schedulerIt = this->schedulerPolingList.begin();
            schedulerIt != this->schedulerPolingList.end(); schedulerIt++) {
+        //若调度器内无事件，则移入等待队列。调度器添加事件时需要唤醒
         if ((*schedulerIt)->getTaskCount() == 0) {
           auto scherdulerPtr = (*schedulerIt);
-          schedulerPolingList.erase(schedulerIt);
+          schedulerIt = schedulerPolingList.erase(schedulerIt);
           schedulerWaitingList.push_back(scherdulerPtr);
           continue;
         }
+        //获取就绪任务列表
         auto [TaskList, interval] = (*schedulerIt)->getReadyTask();
+        //提交任务到线程池
         for (auto task : TaskList) {
           this->pool->submit(task);
         }
+        //更新轮询等待时间，为所有调度器返回的最小值。
         minPollingInterval = std::min(minPollingInterval, interval);
       }
     }
   }
+}
+
+void TaskExcutor::addScheduler(TaskSchedulerImplBase *schedulerPtr) {
+  {
+    std::unique_lock<std::mutex> ul(this->mtx_);
+    this->schedulerWaitingList.remove(schedulerPtr);
+    for (auto scheduler : this->schedulerPolingList) {
+      if (scheduler == schedulerPtr) {
+        return;
+      }
+    }
+    this->schedulerPolingList.push_back(schedulerPtr);
+    // 新任务调度器加入，需要立即查询任务就绪列表，将等待超时时长置0
+    minPollingInterval = 0;
+  }
+  this->cv_.notify_one();
+}
+
+void TaskExcutor::removeScheduler(TaskSchedulerImplBase *schedulerPtr) {
+  std::unique_lock<std::mutex> ul(this->mtx_);
+  this->schedulerWaitingList.remove(schedulerPtr);
+  this->schedulerPolingList.remove(schedulerPtr);
+}
+
+void TaskExcutor::schedulerOnNewTask(TaskSchedulerImplBase *schedulerPtr) {
+  {
+    std::unique_lock<std::mutex> ul(this->mtx_);
+    this->schedulerWaitingList.remove(schedulerPtr);
+    bool alreadyHaveFlag = false;
+    for (auto scheduler : this->schedulerPolingList) {
+      if (scheduler == schedulerPtr) {
+        alreadyHaveFlag = true;
+        break;
+      }
+    }
+    if (!alreadyHaveFlag) {
+      this->schedulerPolingList.push_back(schedulerPtr);
+    }
+    // 新任务加入，需要立即查询任务就绪列表，将等待超时时长置0
+    minPollingInterval = 0;
+  }
+  this->cv_.notify_one();
 }
--- src/TaskExcutor.h
+++ src/TaskExcutor.h
@@ -1,46 +1,65 @@
 #ifndef __TASK_EXCUTOR__
 #define __TASK_EXCUTOR__
 
-#include "ThreadPool/ThreadPool.h"
 #include "TaskScheduler/Impl/TaskSchedulerImplBase.h"
+#include "ThreadPool/ThreadPool.h"
 
+#include <chrono>
 #include <condition_variable>
 #include <functional>
 #include <future>
+#include <list>
 #include <mutex>
 #include <queue>
-#include <list>
 #include <thread>
-#include <chrono>
 
-class TaskExcutor
-{
+class TaskExcutor {
 private:
-    ThreadPool * pool;
-    std::list<TaskSchedulerImplBase*> schedulerPolingList;
-    std::list<TaskSchedulerImplBase*> schedulerWaitingList;
-    std::thread* schedulerThread;
-    std::mutex mtx_;
-    std::condition_variable cv_;
-    int minPollingInterval = INT_MAX;
-    static std::once_flag constructFlag;
-    TaskExcutor():pool(ThreadPool::getInstance()){};
-    void polling();
+  ThreadPool *pool;
+  std::list<TaskSchedulerImplBase *> schedulerPolingList;
+  std::list<TaskSchedulerImplBase *> schedulerWaitingList;
+
+  std::thread *schedulerThread;
+  bool stop = false;
+
+  static std::once_flag constructFlag;
+
+  std::mutex mtx_;
+  std::condition_variable cv_;
+
+  int minPollingInterval = 0;
+
+  TaskExcutor() : pool(ThreadPool::getInstance()){};
+  void polling();
+
 public:
-    TaskExcutor* getInstance() {
-        static TaskExcutor* insPtr = new TaskExcutor();
-        auto instance = insPtr; 
-        std::call_once(this->constructFlag,[instance](){
-            instance->schedulerThread = new std::thread([instance](){
-                instance->polling();
-            });
-            instance->schedulerThread->join();
-        });
+  static TaskExcutor *getInstance() {
+    static TaskExcutor *insPtr = new TaskExcutor();
+    auto instance = insPtr;
+    std::call_once(TaskExcutor::constructFlag, [instance]() {
+      instance->schedulerThread =
+          new std::thread([instance]() { instance->polling(); });
+    });
+    return instance;
+  }
+
+  ~TaskExcutor(){};
+
+  static void stopExcutor() {
+    auto instance = TaskExcutor::getInstance();
+    {
+      std::unique_lock<std::mutex> ul(instance->mtx_);
+      instance->stop = true;
     }
+    instance->cv_.notify_all();
+    instance->schedulerThread->join();
+  }
 
-    ~TaskExcutor();
+  void addScheduler(TaskSchedulerImplBase *);
 
-    void addScheduler(TaskSchedulerImplBase*);
+  void removeScheduler(TaskSchedulerImplBase *);
+
+  void schedulerOnNewTask(TaskSchedulerImplBase *);
 };
 
 #endif
