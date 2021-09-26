aefba5e419ec62e5897b9711da5ddc04f2fdf5c5
lk <448312134@qq.com>
2021-09-26 18:56:00 +0800

    feature: 移除优先级调度器

--- test/test.cpp
+++ test/test.cpp
@@ -1,4 +1,3 @@
-#include "../src/PriorityTaskExcutor.h"
 #include "../src/TimedTaskExcutor.h"
 
 #include <iostream>
@@ -11,13 +10,6 @@ int callBack(int i, int *result) {
   return i;
 }
 
-int callBackP(int i, int priority) {
-  std::cout << "call back! No: " << i << " Priority:" << priority << std::endl;
-  std::this_thread::sleep_for(1000ms);
-  std::cout << "No: " << i << " Priority:" << priority << " Done!" << std::endl;
-  return i;
-}
-
 int main() {
   int i = 0;
   int result = 0;
@@ -26,14 +18,6 @@ int main() {
   auto b = excutor->addTaskWithFuture(14000, callBack, i++, &result);
   auto c = excutor->addTaskWithFuture(7000, callBack, i++, &result);
   auto id = excutor->addTaskWithFuture(10000, ::callBack, i++, &result);
-  PriorityTaskExcutor excutorP;
-
-  excutorP.addTaskWithFuture(9,callBackP,i++,9);
-  auto p = excutorP.addTaskWithFuture(25,callBackP,i++,25);
-  excutorP.addTaskWithFuture(3,callBackP,i++,3);
-  excutorP.addTaskWithFuture(1,callBackP,i++,1);
-
-  excutorP.adjustTask(p.first,24);
 
   excutor->adjustTask(id.first, -5000);
 
