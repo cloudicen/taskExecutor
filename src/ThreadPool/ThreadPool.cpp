277d1bc5bb8688503f2b1788fb0a196b428de35c
cloudicen <cloudicen@foxmail.com>
2021-10-03 20:20:21 +0800

    style：名称统一

--- src/ThreadPool/ThreadPool.cpp
+++ src/ThreadPool/ThreadPool.cpp
@@ -1,13 +1,13 @@
 #include "ThreadPool.h"
 
-std::once_flag ThreadPool::threadPoolConstruct;
+std::once_flag ThreadPool::constructFlag;
 ThreadPool *ThreadPool::instance;
 
 ThreadPool *ThreadPool::getInstance(size_t threadNum) {
   if (ThreadPool::instance != nullptr) {
     return ThreadPool::instance;
   }
-  std::call_once(ThreadPool::threadPoolConstruct, [threadNum]() {
+  std::call_once(ThreadPool::constructFlag, [threadNum]() {
     ThreadPool::instance = new ThreadPool();
     for (size_t i = 0; i < threadNum; ++i) {
       ThreadPool::instance->workers_.emplace_back([]() {
@@ -16,10 +16,10 @@ ThreadPool *ThreadPool::getInstance(size_t threadNum) {
           {
             std::unique_lock<std::mutex> ul(ThreadPool::instance->mtx_);
             ThreadPool::instance->cv_.wait(ul, []() {
-              return ThreadPool::instance->stop_ ||
+              return ThreadPool::instance->stopFlag ||
                      !ThreadPool::instance->tasks_.empty();
             });
-            if (ThreadPool::instance->stop_ &&
+            if (ThreadPool::instance->stopFlag &&
                 ThreadPool::instance->tasks_.empty()) {
               return;
             }
@@ -37,7 +37,7 @@ ThreadPool *ThreadPool::getInstance(size_t threadNum) {
 void ThreadPool::joinAll() {
   {
     std::unique_lock<std::mutex> ul(instance->mtx_);
-    instance->stop_ = true;
+    instance->stopFlag = true;
   }
   instance->cv_.notify_all();
   for (auto &worker : instance->workers_) {
@@ -49,7 +49,7 @@ void ThreadPool::submit(std::function<void()> task) {
   auto instance = ThreadPool::getInstance();
   {
     std::unique_lock<std::mutex> ul(instance->mtx_);
-    if (instance->stop_) {
+    if (instance->stopFlag) {
       throw std::runtime_error("submit on stopped ThreadPool");
     }
     instance->tasks_.emplace(task);
